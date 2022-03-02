ea34da0be95a8807322ed1b2063675631ee64ab7
Hai Huang <illiteratehuang@gmail.com>
2022-03-02 11:46:09 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,42 +13,41 @@ st.sidebar.subheader("Visualization Settings")
 upload_file = st.sidebar.file_uploader(label="Drop Bank Statement PDF file", type=['pdf'])
 
 @st.cache
+def load_pdf():
+    if upload_file is not None:
+        print(upload_file)
+        try:
+            df_list = tabula.read_pdf(upload_file, stream=True, pages=[3,4], guess=False)
+            df = pd.DataFrame(df_list[0])
+            df.rename(columns={'Unnamed: 0': 'PURCHASES', 'ONLINE PHONE': 'MERCHANT CATEGORY', 'Unnamed: 1': 'AMOUNT'}
+                      , inplace=True)
+            df = df[['PURCHASES', 'MERCHANT CATEGORY', 'AMOUNT']]
+            df = df.dropna()
+            df = df.iloc[1:, :]
+            df = df.reset_index(drop=True)
+            df['DATE'] = df['PURCHASES'].str[:5]
+            df['PURCHASES'] = df['PURCHASES'].str[5:]
 
-global df_final
+            df_1 = pd.DataFrame(df_list[1])
+            df_1.rename(columns={'DISCOVER IT® CARD ENDING IN 1994': 'PURCHASES', 'Unnamed: 0': 'MERCHANT CATEGORY',
+                                 'Unnamed: 1': 'AMOUNT'}, inplace=True)
+            df_1 = df_1[['PURCHASES', 'MERCHANT CATEGORY', 'AMOUNT']]
+            df_1 = df_1.dropna()
+            df_1 = df_1.loc[(df_1['PURCHASES'].str.contains('/'))]
+            df_1 = df_1.reset_index(drop=True)
+            df_1['DATE'] = df_1['PURCHASES'].str[:5]
+            df_1['PURCHASES'] = df_1['PURCHASES'].str[5:]
 
-if upload_file is not None:
-    print(upload_file)
-    try:
-        df_list = tabula.read_pdf(upload_file, stream=True, pages=[3,4], guess=False)
-        df = pd.DataFrame(df_list[0])
-        df.rename(columns={'Unnamed: 0': 'PURCHASES', 'ONLINE PHONE': 'MERCHANT CATEGORY', 'Unnamed: 1': 'AMOUNT'}
-                  , inplace=True)
-        df = df[['PURCHASES', 'MERCHANT CATEGORY', 'AMOUNT']]
-        df = df.dropna()
-        df = df.iloc[1:, :]
-        df = df.reset_index(drop=True)
-        df['DATE'] = df['PURCHASES'].str[:5]
-        df['PURCHASES'] = df['PURCHASES'].str[5:]
-
-        df_1 = pd.DataFrame(df_list[1])
-        df_1.rename(columns={'DISCOVER IT® CARD ENDING IN 1994': 'PURCHASES', 'Unnamed: 0': 'MERCHANT CATEGORY',
-                             'Unnamed: 1': 'AMOUNT'}, inplace=True)
-        df_1 = df_1[['PURCHASES', 'MERCHANT CATEGORY', 'AMOUNT']]
-        df_1 = df_1.dropna()
-        df_1 = df_1.loc[(df_1['PURCHASES'].str.contains('/'))]
-        df_1 = df_1.reset_index(drop=True)
-        df_1['DATE'] = df_1['PURCHASES'].str[:5]
-        df_1['PURCHASES'] = df_1['PURCHASES'].str[5:]
-
-        df_final = pd.concat([df, df_1])
-        df_final = df_final.reset_index(drop= True)
-        df_final = df_final[['DATE', 'PURCHASES', 'MERCHANT CATEGORY', 'AMOUNT']]
-        df_final.AMOUNT = df_final.AMOUNT.apply(lambda x: float(x[1:]))
-    except Exception as e:
-        print('The Error is', e)
-
+            df_final = pd.concat([df, df_1])
+            df_final = df_final.reset_index(drop= True)
+            df_final = df_final[['DATE', 'PURCHASES', 'MERCHANT CATEGORY', 'AMOUNT']]
+            df_final.AMOUNT = df_final.AMOUNT.apply(lambda x: float(x[1:]))
+            return df_final
+        except Exception as e:
+            print('The Error is', e)
+dff = load_df() 
 try:
-    st.write(df_final)
+    st.write(dff)
     st.text("")
     st.text("")
 except Exception as e:
@@ -61,21 +60,21 @@ chart_select = st.sidebar.selectbox(
     
 if chart_select == 'Pie Plot':
     try:
-        plot = px.pie(df_final, values="AMOUNT", names="MERCHANT CATEGORY", width=800, height= 800)
+        plot = px.pie(dff, values="AMOUNT", names="MERCHANT CATEGORY", width=800, height= 800)
         plot.update_traces(textposition="inside", textinfo= 'percent+label')
         st.plotly_chart(plot)
         st.subheader("Table of total cost in each category")
-        stat_t = df_final.groupby(by='MERCHANT CATEGORY')['AMOUNT'].sum()
+        stat_t = dff.groupby(by='MERCHANT CATEGORY')['AMOUNT'].sum()
         st.dataframe(stat_t)
     except Exception as e:
         print('The Error is', e)
 elif chart_select == 'Statistic Table':
     try:
         st.subheader("Table of total cost in each category")
-        stat_t = df_final.groupby(by='MERCHANT CATEGORY')['AMOUNT'].sum()
+        stat_t = dff.groupby(by='MERCHANT CATEGORY')['AMOUNT'].sum()
         st.dataframe(stat_t)
         st.write("")
-        total_cost = df_final['AMOUNT'].sum()
+        total_cost = dff['AMOUNT'].sum()
         st.subheader("Total Cost of this Month is:  ${}".format(total_cost))
     except Exception as e:
         print('The Error is', e)
@@ -84,7 +83,7 @@ elif chart_select == 'Download as CSV':
         @st.cache
         def convert_df(df):
             return df.to_csv(index= False).encode('utf-8')
-        csv = convert_df(df_final)
+        csv = convert_df(dff)
         st.download_button(
             "Press to Download",
             csv,
